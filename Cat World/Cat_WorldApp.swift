c2d27ddc070691a1f739156497655a67d837d5ae
Yusuf <ybkocaman@gmail.com>
2024-04-24 14:22:34 +0100

    Initial Commit

---
+++ Cat World/Cat_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_WorldApp.swift
+//  Cat World
+//
+//  Created by Yusuf Burak on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cat_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
